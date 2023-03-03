48e775e889dc90d37048163dd6d2737c98d68c3c
Lohita Sai Palathirthapu <76088006+lohita23@users.noreply.github.com>
2023-03-04 03:29:43 +0530

    questions update

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the most popular coffee drink in the world?",
+    answer: "Americano",
+    options: [
+      "Cappuccino",
+      "Espresso",
+      "Latte",
+      "Americano"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which country is the largest producer of coffee in the world?",
+    answer: "Brazil",
+    options: [
+      "Brazil",
+      "Colombia",
+      "Ethiopia",
+      "Vietnam"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the name of the coffee plant?",
+    answer: "Coffea arabica",
+    options: [
+      "Coffea arabica",
+      "Coffea canephora",
+      "Coffea liberica",
+      "Coffea excelsa"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the process of removing the outer layer of coffee beans called?",
+    answer: "Hulling",
+    options: [
+      "Roasting",
+      "Grinding",
+      "Decaffeination",
+      "Hulling"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which coffee brewing method is known for producing a strong, thick coffee?",
+    answer: "Turkish coffee",
+    options: [
+      "Espresso",
+      "Drip coffee",
+      "Turkish coffee",
+      "Moka pot",
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
